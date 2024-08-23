c8cb9f6064262dcd26c66f09c7d8dbcdf3b5918f
HZ2049 <huzeyfaaseid@gmail.com>
2024-08-23 12:25:50 -0700

    errorHandling

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,38 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [city, setCity] = useState("")
-  const [weather, setWeather]= useState(null)
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState();
 
-  const apiKey= import.meta.env.VITE_WEATHER_API_KEY
-  
-  
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-  const getWeather= async () => {
-    const response = await axios.get (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-  console.log("response", response)
-  setWeather(response.data)
-  }
-
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+      console.log("response", response);
+      setWeather(response.data);
+      setError("");
+    } catch (error) {
+      setError("Not Found");
+      setWeather(null);
+    }
+  };
 
   return (
     <div>
-      {weather && 
-      <h1>{weather.main.temp}</h1>
-      }
-      
+      {weather && <h1>{weather.main.temp}</h1>}
+      {error && <h1>{error}</h1>}
+      <input
+        type="text"
+        placeholder="Enter City Name"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+      />
 
-      <input type="text" placeholder="Enter City Name" onChange={(e)=>setCity(e.target.value)} value={city}/>
-
-      <button onClick={getWeather} >Get Weather</button>
+      <button onClick={getWeather}>Get Weather</button>
     </div>
   );
 }
