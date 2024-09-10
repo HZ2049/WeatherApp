7a98c46f271cdcdbf3c7c52fbe4cba2adb474f12
HZ2049 <huzeyfaaseid@gmail.com>
2024-09-10 13:38:18 -0700

    background images have been added

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,67 @@ function App() {
     }
   };
 
+  let backgroundImage
+  if(weather){
+    switch(weather.weather[0].main){
+      case "Clouds":
+        backgroundImage="url('Cloudy.jpg')"
+        break
+      
+      case "Clear":
+        backgroundImage="url('Sunny.jpg')"
+        break
+      case"Snow":
+        backgroundImage="url('Snow.jpg')"
+        break
+      case"Rain":
+        backgroundImage="url('Rainy.jpg')"
+        break
+      
+        default:
+          backgroundImage="url('Sunny.jpg')"
+          
+    }
+  } else{
+    backgroundImage="url('Snow.jpg')"
+
+  }
+  
+
   return (
+    <div style={{
+      backgroundImage:backgroundImage,
+      height:"100vh",
+      width:"100vh",
+      backgroundSize:"cover",
+      backgroundPosition:"center",
+      display:"flex",
+      flexDirection:"row",
+      justifyContent:"center",
+      alignItems:"center"
+
+    }}>
+      
     <div>
-      {weather && <h1>{weather.main.temp}</h1>}
+      
+      {weather && <div> <h1>Temperature:{weather.main.temp}</h1> 
+      <h2>Humudity:{weather.main.humidity}</h2> 
+      </div>}
       {error && <h1>{error}</h1>}
-      <input
+
+      <div style={{
+          padding:"10px"
+          
+      }}>
+        <input
+        style={{
+        height:"38px",
+        padding:"2px",
+        marginRight:"20px",
+        borderRadius:"8px",
+        border:"1px solid #ddd",
+        fontSize:"18px"
+        }}
         type="text"
         placeholder="Enter City Name"
         onChange={(e) => setCity(e.target.value)}
@@ -37,6 +93,9 @@ function App() {
       />
 
       <button onClick={getWeather}>Get Weather</button>
+      </div>
+
+    </div>
     </div>
   );
 }
